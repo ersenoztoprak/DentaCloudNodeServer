a0b9bef36c0c3847be1102a94d492711a44d92c6
Ersen Öztoprak <hersenoztoprak@yahoo.com>
2016-05-21 01:50:39 +0300

    refactored CORS solution

--- app.js
+++ app.js
@@ -42,26 +42,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-access-token');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
+var cors = require('cors');
+app.use(cors());
+app.options('*', cors());
 // passport config
 var User = require('./models/user');
 app.use(passport.initialize());
