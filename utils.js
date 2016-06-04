d10728d29dcaee6a4c00a901ab38e378822ace26
Ersen Öztoprak <hersenoztoprak@yahoo.com>
2016-06-04 18:43:07 +0300

    fixed end date of the week

--- utils.js
+++ utils.js
@@ -2,11 +2,10 @@ exports.upcomingDatesOfWeek = function () {
 
   var now = new Date();
 
-  // set time to some convenient value
-  now.setHours(0,0,0,0);
-
   // Get next Sunday
   var sunday = new Date(now);
+  // set time to some convenient value
+  sunday.setHours(23,59,59,59);
   sunday.setDate(sunday.getDate() - sunday.getDay() + 7);
 
   return [Math.round(now.getTime()/1000), Math.round(sunday.getTime()/1000)];
